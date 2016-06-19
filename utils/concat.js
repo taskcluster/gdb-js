2c5b048916e5575409ecb99bea37e4b112c4042e
Alexander Baygeldin <a.baygeldin@gmail.com>
2016-06-19 22:44:27 +0300

    Update concat.js

--- utils/concat.js
+++ utils/concat.js
@@ -4,7 +4,7 @@ import fs from 'fs'
 let res = []
 
 for (let file of fs.readdirSync(process.argv[2])) {
-  let name = [file.slice(0, -3)]
+  let name = file.slice(0, -3)
   let src = fs.readFileSync(path.join(process.argv[2], file)).toString()
   res.push({ name, src })
 }
